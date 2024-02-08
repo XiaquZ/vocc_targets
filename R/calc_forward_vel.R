21092d123f2f85d78320ae50ba4742a7a66309e4
vsc34871 <vsc34871@tier2-p-login-1.genius.hpc.kuleuven.be>
2024-02-08 15:05:52 +0100

    from hpc

---
+++ R/calc_forward_vel.R
@@ -0,0 +1,51 @@
+calc_forward_vel <- function(tile_name,
+                             tolerance,
+                             max_distance,
+                             present_files, # must contain
+                             future_files) {
+  print(paste("Now calculating:", tile_name))
+
+  ## Load data
+  pre <- rast(grep(tile_name, present_files, value = T))
+  ## names(pre) <- "pre"
+  fut <- rast(grep(tile_name, future_files, value = T))
+  ## names(fut) <- "fut"
+
+  ## Round pre and fut to one decimal
+  pre_round <- round(pre, 1)
+  pre_values <- freq(pre_round, digits = 1)$value
+  fut_round <- round(fut, 1)
+
+  ## Set tolerace for matching analogues
+
+  ## apply over all of the pre_values in lis
+  analogue_distances <- lapply(pre_values, function(pre_value) {
+    ## Filter only values in pre_round that are equal to pre_value
+    pre_filt <- mask(pre_round,
+      pre_round == pre_value,
+      maskvalues = F
+    )
+
+    ## Filter future analogues which are within tolerance of pre_value
+    fut_filt <- mask(fut_round,
+      fut_round >= pre_value - tolerance & fut_round <= pre_value + tolerance,
+      maskvalues = F
+    )
+    # Calculate distance to closest analogue
+    fut_distance <- distance(fut_filt)
+    ## Crop to tile to exclude buffer around tile
+    analogue_distance <- mask(crop(fut_distance, pre_filt), pre_filt)
+
+    ## all_analogue_distance <- sum(all_analogue_distance, analogue_distance, na.rm = T)
+  })
+
+  ds <- rast(analogue_distances) # List of all analogue rasts to one rast
+  distance <- app(ds, fun = sum, na.rm = T) # Sum all layers of ds rast to make complete map
+  names(distance) <- "distance"
+
+  forward_vel_file <- paste0("/lustre1/scratch/348/vsc34871/output/VoCC/NorthEU/", tile_name, ".tif")
+  forward_vel <- mask(distance, distance <= max_distance, maskvalues = F) / 75 # Calculate velocity
+  writeRaster(forward_vel, forward_vel_file, overwrite = T) # write
+
+  return(forward_vel_file) # Return filename
+}
---
+++ _targets.R
@@ -0,0 +1,57 @@
+library(targets)
+library(tarchetypes)
+library(clustermq)
+
+## ### Running locally
+## # Sets up how multiple targets are run parallel using the clustermq package
+## options(clustermq.scheduler = "multiprocess")
+
+### Running on HPC
+## Settings for clustermq
+options(
+  clustermq.scheduler = "slurm",
+  clustermq.template = "./cmq.tmpl" # if using your own template
+)
+
+## Settings for clustermq template when running clustermq on HPC
+tar_option_set(
+  resources = tar_resources(
+    clustermq = tar_resources_clustermq(template = list(
+      job_name = "auto-velocity",
+      per_cpu_mem = "15000mb",
+      n_tasks = 1,
+      per_task_cpus = 5,
+      walltime = "00:55:00"
+    ))
+  )
+)
+
+# Loads all R scripts in ./R/ directory.
+# Here, all packages are loaded from the ./R/packages.R file
+# The calc_forward_vel function is loaded from the ./R/calc_forward_vel.R file
+tar_source()
+
+tar_plan(
+  tolerance = 0.25,
+  max_distance = 30000,
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/pre", full.names = T),
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/fut", full.names = T),
+  tar_target(tile_names,
+    paste0(paste0(str_split(
+      gsub("ForestMAT_", "", tail(str_split(present_files, "/")[[1]], 1)),
+      "_"
+    )[[1]][1:2], collapse = "_"), "_"),
+    pattern = map(present_files),
+  ),
+  tar_target(forward_vels,
+    calc_forward_vel(
+      tile_names,
+      tolerance,
+      max_distance,
+      present_files,
+      future_files
+    ),
+    pattern = map(tile_names),
+    format = "file"
+  )
+)
