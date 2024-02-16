a13392abe65e2579f6ee177bb4482e9851bce161
XiaquZ <xiaqu.zhou@gmail.com>
2024-02-16 11:31:29 +0100

    test bigmen on wice

--- _targets.R
+++ _targets.R
@@ -18,7 +18,7 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "20000mb",
+      per_cpu_mem = "30000mb",
       n_tasks = 1,
       per_task_cpus = 36,
       walltime = "24:00:00"
