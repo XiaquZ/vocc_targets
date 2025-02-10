3b0f685bd4235e4f0c2e051900f9fd265ab9ceec
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-10 16:56:24 +0100

    Modify targets workflow to calculate the FVoMC with 100km searching radius. Future tiles are buffer rings.

--- _targets.R
+++ _targets.R
@@ -13,12 +13,15 @@ options(
   clustermq.template = "./cmq.tmpl" # if using your own template
 )
 
+## Running locally on Windows
+options(clustermq.scheduler = "multiprocess")
+
 ## Settings for clustermq template when running clustermq on HPC
 tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "20000mb",
+      per_cpu_mem = "5000mb",
       n_tasks = 1,
       per_task_cpus = 36,
       walltime = "15:00:00"
@@ -33,12 +36,12 @@ tar_source()
 
 tar_plan(
   tolerance = 0.25,
-  max_distance = 75000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preIT/", full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futIT/", full.names = T),
+  max_distance = 100000,
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preFr/", full.names = T),
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futFr/", full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
-      gsub("ForestMAT_", "", tail(str_split(present_files, "/")[[1]], 1)),
+      gsub("bio1_pre_SA_", "", tail(str_split(present_files, "/")[[1]], 1)),
       "_"
     )[[1]][1:2], collapse = "_"), "_"),
     pattern = map(present_files),
