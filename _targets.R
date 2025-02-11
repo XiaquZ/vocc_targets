b0497f46ebc50f5e587175cb312d7de53a862d98
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-11 12:24:17 +0100

    France tile 9 fvomc on wice cluster.

--- _targets.R
+++ _targets.R
@@ -21,9 +21,9 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "7000mb",
+      per_cpu_mem = "4000mb",
       n_tasks = 1,
-      per_task_cpus = 36,
+      per_task_cpus = 72,
       walltime = "15:00:00"
     ))
   )
