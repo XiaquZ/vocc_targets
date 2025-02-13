d9f51f18ae880414da2714807f50945f305b3a3c
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-13 14:33:39 +0100

    update.

--- _targets.R
+++ _targets.R
@@ -21,9 +21,9 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "3300mb",
+      per_cpu_mem = "5000mb",
       n_tasks = 1,
-      per_task_cpus = 72,
+      per_task_cpus = 36,
       walltime = "18:00:00"
     ))
   )
