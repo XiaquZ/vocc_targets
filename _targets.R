79e5d27068e3889715769ee8f7adaa8301ce7893
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-17 14:50:16 +0100

    Start calculating FVoMC 100km for Italy.

--- _targets.R
+++ _targets.R
@@ -37,9 +37,9 @@ tar_source()
 tar_plan(
   tolerance = 0.25,
   max_distance = 100000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preEastEU/",
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preIT/",
    full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futEastEU/",
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futIT/",
    full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
