ac60b2ca4935b63807e03c07ab40832a8c0ce205
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-17 15:14:49 +0100

    Start calculating FVoMC 100km buffer ring for Northeast EU.

--- _targets.R
+++ _targets.R
@@ -37,9 +37,9 @@ tar_source()
 tar_plan(
   tolerance = 0.25,
   max_distance = 100000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preIT/",
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preNE/",
    full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futIT/",
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futNE/",
    full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
