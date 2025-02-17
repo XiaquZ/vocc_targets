2de66362a2383605a438d625986487eb4179b897
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-17 17:44:06 +0100

    EastEU resubmit.

--- _targets.R
+++ _targets.R
@@ -21,7 +21,7 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "20000mb",
+      per_cpu_mem = "5000mb",
       n_tasks = 1,
       per_task_cpus = 36,
       walltime = "18:00:00"
@@ -37,9 +37,9 @@ tar_source()
 tar_plan(
   tolerance = 0.25,
   max_distance = 100000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preNE/",
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preEastEU/",
    full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futNE/",
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futEastEU/",
    full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
