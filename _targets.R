4b6808a45a209c1c824d3f00339d379076b54c60
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-19 17:03:11 +0100

    Update PTES

--- _targets.R
+++ _targets.R
@@ -37,9 +37,9 @@ tar_source()
 tar_plan(
   tolerance = 0.25,
   max_distance = 100000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preNorth/",
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/prePTES/",
    full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futNorth/",
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futPTES/",
    full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
