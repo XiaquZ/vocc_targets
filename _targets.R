9dd7458027baf3797a202fd70916f508614623a4
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-24 16:31:41 +0100

    switch to bigmem for East EU.

--- _targets.R
+++ _targets.R
@@ -17,7 +17,7 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "5000mb",
+      per_cpu_mem = "20000mb",
       n_tasks = 1,
       per_task_cpus = 36,
       walltime = "20:00:00"
