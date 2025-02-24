84e71b3bfc6901d1818bf2fd37ca13a25eb3f066
XiaquZ <xiaqu.zhou@gmail.com>
2025-02-24 15:18:20 +0100

    East EU fvomc 100km

--- _targets.R
+++ _targets.R
@@ -17,9 +17,9 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-velocity",
-      per_cpu_mem = "3000mb",
+      per_cpu_mem = "5000mb",
       n_tasks = 1,
-      per_task_cpus = 72,
+      per_task_cpus = 36,
       walltime = "20:00:00"
     ))
   )
@@ -33,9 +33,9 @@ tar_source()
 tar_plan(
   tolerance = 0.25,
   max_distance = 100000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/prePTES/",
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/preEastEU/",
    full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futPTES/",
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/VoCC/futEastEU/",
    full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
