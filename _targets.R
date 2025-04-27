14879915e795093dbf9223d3e85316723821294e
XiaquZ <xiaqu.zhou@gmail.com>
2025-04-27 15:43:46 +0200

    central EU

--- _targets.R
+++ _targets.R
@@ -34,11 +34,11 @@ tar_plan(
   tolerance = 0.25,
   max_distance = 75000,
   present_files = list.files(
-    "/lustre1/scratch/348/vsc34871/input/FVoMC/preEastEU/",
+    "/lustre1/scratch/348/vsc34871/input/FVoMC/preCentralEU/",
     full.names = T
   ),
   future_files = list.files(
-    "/lustre1/scratch/348/vsc34871/input/FVoMC/futEastEU/",
+    "/lustre1/scratch/348/vsc34871/input/FVoMC/futCentralEU/",
     full.names = T
   ),
   tar_target(tile_names,
